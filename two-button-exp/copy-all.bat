57a61589632b4393a2019206991b90f8391c232d
Rick Neeft <rick@chroomsoft.nl>
2021-09-11 15:36:47 +0200

    Commit the two-button expirement

---
+++ two-button-exp/copy-all.bat
@@ -0,0 +1,3 @@
+copy "C:\Users\RickNeeft\source\repos\Expirements\exp_v1\bin\Debug\exp_v1.exe" exp_v1.exe
+copy "C:\Users\RickNeeft\source\repos\Expirements\exp_v2\bin\Debug\exp_v2.exe" exp_v2.exe
+copy "C:\Users\RickNeeft\source\repos\Expirements\exp_v3\bin\Debug\exp_v3.exe" exp_v3.exe
---
+++ two-button-exp/exp_v2/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Expirement
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this, "Hello world version 1");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this, "Welcome new version!");
+        }
+    }
+}
